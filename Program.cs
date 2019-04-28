94cb848a6af8f8b229ee8445133a3f80ddb269e1
Tamozhnii <petek379@ukr.net>
2019-04-28 23:56:25 +0300

    Task_1

--- Program.cs
+++ Program.cs
@@ -8,8 +8,24 @@ namespace Algoritm
 {
     class Program
     {
+        static string BiNumber(int i, ref string j)
+        {
+            if (i > 0)
+            {
+                BiNumber(i / 2, ref j);
+                j += (i % 2).ToString();
+            }
+            if (i == 0)
+            {
+                return j;
+            }
+            return j;
+        }
         static void Main(string[] args)
         {
+            string biNumber = null;
+            Console.Write(BiNumber(25, ref biNumber));
+            Console.ReadKey();
         }
     }
 }
