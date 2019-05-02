2f8c2f3d320e2ea84388831f62d8c9dc2764158a
Tamozhnii <petek379@ukr.net>
2019-05-02 12:04:52 +0300

    Task_3

--- Program.cs
+++ Program.cs
@@ -57,7 +57,7 @@ namespace Algoritm
                     }
                 } return 1;
             } return 0;
-        }//Степень рекурсией
+        } //Степень рекурсией
 
         static int NewPow(int a, int count)
         {
@@ -83,17 +83,54 @@ namespace Algoritm
                 return 1;
             }
             return 0;
-        }//Степень рекурсией со свойствами четности
+        } //Степень рекурсией со свойствами четности
+
+        static int CountRoad(int a, int b)
+        {
+            int count = 0;
+            List<int> list = new List<int>() { a };
+            for (int i = 0; i < list.Count; i++)
+            {
+                if(list[i] < b)
+                {
+                    list.Add(list[i] + 1);
+                    list.Add(list[i] * 2);
+                }
+                else if (list[i] == b)
+                {
+                    count++;
+                }
+            }
+            return count;
+        } //Количество ходов калькулятора с помощью массива
+
+        static void CountRoadRec(int a, int b, ref int count)
+        {
+            if(a < b)
+            {
+                int a1 = a + 1;
+                if (a1 < b) CountRoadRec(a1, b, ref count);
+                else if(a1 == b) count++;
+                int a2 = a * 2;
+                if (a2 < b) CountRoadRec(a2, b, ref count);
+                else if (a2 == b) count++;
+            }
+            else if (a == b) count++;
+        } //Количество ходов калькулятора с помощью рекурсии
+
         static void Main(string[] args)
         {
             string biNumber = null;
-            Console.WriteLine(BiNumber(25, ref biNumber));
+            Console.WriteLine(BiNumber(25, ref biNumber));//Задание 1
 
-            Console.WriteLine(MyPow(-2, 3));
+            Console.WriteLine(MyPow(-2, 3));//Задание 2
             Console.WriteLine(RecPow(-2, 3));
             Console.WriteLine(NewPow(-2, 9));
 
-
+            Console.WriteLine(CountRoad(1, 2));//Задание 3
+            int count = 0;
+            CountRoadRec(1, 2, ref count);
+            Console.WriteLine(count);
 
             Console.ReadKey();
         }
