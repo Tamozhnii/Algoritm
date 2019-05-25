18e7adaa40607b733938b2f7a8c9e2d0d2d1afab
Tamozhnii <petek379@ukr.net>
2019-05-25 19:17:43 +0300

    step_2

--- Program.cs
+++ Program.cs
@@ -73,10 +73,83 @@ namespace Algoritm                         //Таможний П.И.
 
         static void Infix(string formula)
         {
+            List<char> backetFormul;
+            List<char> operand;
+            List<string> postfixFormul;
             if (Bracket(formula))
             {
-
-
+                foreach (char i in formula)
+                {
+                    if (i == '(')
+                    {
+                        int count = 0;
+                        backetFormul = new List<char>();
+                        do
+                        {
+                            backetFormul.Add(i);
+                            switch (i)
+                            {
+                                case '(':
+                                    count++;
+                                    break;
+                                case ')':
+                                    count--;
+                                    break;
+                                default:
+                                    break;
+                            }
+                        } while (count > 0);
+                        backetFormul.RemoveAt(0);
+                        int last = backetFormul.Count - 1;
+                        backetFormul.RemoveAt(last);
+                        Infix(backetFormul.ToString());
+                    }
+                }
+                foreach (char i in formula)
+                {
+                    postfixFormul = new List<string>();
+                    if(i == '^')
+                    {
+                        int a = formula.LastIndexOf(i);
+                        operand = new List<char>();
+                        for (int g = a + 2; g < formula.Length; g++)
+                        {
+                            if (formula[g] != ' ')
+                            {
+                                operand.Add(formula[g]);
+                            }
+                            else
+                            {
+                                string operandNumber = null;
+                                foreach (char item in operand)
+                                {
+                                    operandNumber += item;
+                                }
+                                postfixFormul.Add($"{operandNumber} ");
+                                operand.Clear();
+                                break;
+                            }
+                        }
+                        for (int j = a - 2; j >= 0; j--)
+                        {
+                            if(formula[j] != ' ')
+                            {
+                                operand.Add(formula[j]);
+                            }
+                            else
+                            {
+                                string operandNumber = null;
+                                foreach(char item in operand)
+                                {
+                                    operandNumber += item;
+                                }
+                                postfixFormul.Add($"{operandNumber} ");
+                                operand.Clear();
+                                break;
+                            }
+                        }
+                    }
+                }
             }
             else Console.WriteLine("Error formula");
         }
