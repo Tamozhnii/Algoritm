9a50c2678c7212211b0c9543d3863925ec962560
Tamozhnii <petek379@ukr.net>
2019-06-02 20:30:47 +0300

    step_2

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Algoritm                              //Таможний Петр
 {
-    class Program
+    class Program               //https://docs.google.com/document/d/1m-tboNAl7OLhX40rw9mWGAts5Io74xKiKyo6Hjn8qAw/edit
     {
         static void Main(string[] args)
         {
