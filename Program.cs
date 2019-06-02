347e95465e5efbd97ab3de589c47f9f3fff8de5a
Tamozhnii <petek379@ukr.net>
2019-06-02 20:20:43 +0300

    step_1

--- Program.cs
+++ Program.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Algoritm
+namespace Algoritm                              //Таможний Петр
 {
     class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
